18166f0c913e0f51753b2f2fb8683f2adae12089
ledwindra <lukman.edwindra@gmail.com>
2021-10-18 20:08:12 +0700

    we can now get each ruling at case-by-case level. added sample data

--- rulings/scraper.py
+++ rulings/scraper.py
@@ -15,7 +15,9 @@ class Content:
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def get_status(self, message, url):
-        """Arguments:
+        """Returns a response status code
+
+        Arguments:
         - message: the activity added with an emoji
         - url: link to the website
         """
@@ -46,12 +48,23 @@ class Content:
         bs = BeautifulSoup(response.content, features="html.parser")
 
         return bs
+
+    def save_to_json(self, data, output):
+        """Save output into a JSON format
+        """
+        filename = f"{output}.json"
+        with open(filename, "w") as f:
+            json.dump(data, f, indent=4)
+        self.get_status("[SAVE \U00002705]", filename)
         
 
 class Aggregate(Content):
 
     def direktori(self, proxies=None):
         """Returns total rulings by "Direktori" (e.g. Perdata Agama, Pidana Umum, etc)
+
+        Argument:
+        - proxies: a string of socks5. Reference: https://2.python-requests.org/en/master/user/advanced/#socks
         """
         response = self.get_response(self.BASE_URL, proxies)
         bs = self.parse_content(response)
@@ -69,6 +82,9 @@ class Aggregate(Content):
 
     def peradilan(self, proxies=None):
         """Returns total rulings by "Peradilan" (e.g. Mahkamah Agung, Peradilan Umum, etc)
+
+        Argument:
+        - proxies: a string of socks5. Reference: https://2.python-requests.org/en/master/user/advanced/#socks
         """
         response = self.get_response(self.BASE_URL, proxies)
         bs = self.parse_content(response)
@@ -80,7 +96,7 @@ class Aggregate(Content):
             total = int(b.text.split()[-1])
             data.append({"peradilan": overall, "total": total})
 
-        self.save_to_json(data, "data/peradilan.json")
+        self.save_to_json(data, "data/peradilan")
 
         return data
 
@@ -89,6 +105,9 @@ class Aggregate(Content):
             - Court name, e.g. "PN Jakarta Barat", "PN Kudus", etc. Empty court name means all courts
             - Type of ruling: either "putus", "register", "upload"
             - Year: self-explanatory
+
+        Argument:
+        - proxies: a string of socks5. Reference: https://2.python-requests.org/en/master/user/advanced/#socks
         """
         select_value = lambda text: [x for x in text if x != ""]
         dictionary = lambda pengadilan, based, data: {
@@ -122,6 +141,9 @@ class Aggregate(Content):
     
     def klasifikasi(self, proxies=None):
         """Returns total rulings by klasifikasi for each direktori, e.g. Perceraian, Pengesahan Nikah from Perdata Agama
+
+        Argument:
+        - proxies: a string of socks5. Reference: https://2.python-requests.org/en/master/user/advanced/#socks
         """
         dictionary = lambda direktori, data: {
             "direktori": direktori,
@@ -154,6 +176,9 @@ class Aggregate(Content):
             - Court name, e.g. "PN Jakarta Barat", "PN Kudus", etc. Empty court name means all courts
             - Type of ruling: either "putus", "register", "upload"
             - Year: self-explanatory
+
+        Argument:
+        - proxies: a string of socks5. Reference: https://2.python-requests.org/en/master/user/advanced/#socks
         """
         select_value = lambda text: [x for x in text if x != ""]
         dictionary = lambda _direktori, pengadilan, based, data: {
@@ -192,32 +217,350 @@ class Aggregate(Content):
 
         return _periode_klasifikasi
 
-    def save_to_json(self, data, output):
-        filename = f"{output}.json"
-        with open(filename, "w") as f:
-            json.dump(data, f, indent=4)
-        self.get_status("[SAVE \U00002705]", filename)
-
 
 class Case(Content):
 
-    def find_portfolio(self, bs):
-         return bs.find_all('ul', {'class': 'portfolio-meta nobottommargin'})
+    def total_page(self, proxies=None):
+        """Returns total page (integer)
 
-    def find_a(self, portofolio):
-        return [x.find_all('a') for x in portofolio]
+        Argument:
+        - proxies: a string of socks5. Reference: https://2.python-requests.org/en/master/user/advanced/#socks
+        """
+        response = self.get_response("https://putusan3.mahkamahagung.go.id/direktori.html", proxies)
+        bs = self.parse_content(response)
+        css = ".pagination"
+        page = int(bs.select(css)[0].find_all("a")[-1]["data-ci-pagination-page"])
 
-    def get_href(self, a):
-        for i in a:
-            for j in i:
-                if 'pdf' in j['href']:
-                    return j['href']
+        return page
 
-    def download_pdf(self, href, output='tes'):
-        return os.system(f'wget {href} -O {output}.pdf --no-check-certificate --user-agent="{self.headers["User-Agent"]}"')
+    def case(self, proxies=None, total_page=5):
+        """Returns each ruling URL for every page.
 
-    def pdf_to_txt(self, pdf_file='tes'):
-        return os.system(f'pdftotext {pdf_file}.pdf')
+        Arguments:
+        - total_page: number of pages that will be scraped.
+        Default value is five pages, which results to almost 300 rulings
+        """
+        i = 1
+        last_page = self.total_page(proxies)
+        if total_page != last_page:
+            last_page = total_page
+        data = []
+        while i <= last_page:
+            url = f"https://putusan3.mahkamahagung.go.id/direktori/index/page/{i}.html"
+            response = self.get_response(url, proxies)
+            bs = self.parse_content(response)
+            bs = bs.select(".tab-container")[0]
+            a = bs.find_all("a")
+            href = [x["href"] for x in a]
+            for h in href:
+                if "/putusan/" in h:
+                    data.append(h)
+            i += 1
+            
+        return data
+
+    def putusan(self, bs):
+        """Returns title of a particular ruling
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > h2:nth-child(1)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+
+        return output
+
+    def nomor(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+
+        return output
+
+    def tingkat_proses(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def klasifikasi(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def kata_kunci(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+
+        return output
+
+    def tahun(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(6) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def tanggal_register(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def lembaga_peradilan(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(8) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+
+        return output
+
+    def jenis_lembaga_peradilan(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(9) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def hakim_ketua(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(10) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def hakim_anggota(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(11) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+    
+    def panitera(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(12) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def amar(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(13) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+    
+    def amar_lainnya(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(14) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def catatan_amar(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(15) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def tanggal_musyawarah(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(16) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def tanggal_dibacakan(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(17) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def kaidah(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(18) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+    
+    def abstrak(self, bs):
+        """Returns
+        """
+        try:
+            css = ".table > tbody:nth-child(1) > tr:nth-child(19) > td:nth-child(2)"
+            output = bs.select(css)[0].text.upper()
+            output = re.sub("[\r\n\t]", "", output)
+            output = re.sub(" +", " ", output)
+        except Exception as e:
+            print(e)
+            output = None
+        
+        return output
+
+    def create_json(self, data, proxies=None):
+        """Returns final output of each ruling in JSON format
+
+        Arguments:
+        - data: the output from `case` method
+        - proxies: a string of socks5. Reference: https://2.python-requests.org/en/master/user/advanced/#socks
+        """
+        _json = []
+        for url in data:
+            response = self.get_response(url, proxies)
+            bs = self.parse_content(response)
+            d = {
+                "putusan": self.putusan(bs),
+                "nomor": self.nomor(bs),
+                "tingkat_proses": self.tingkat_proses(bs),
+                "klasifikasi": self.klasifikasi(bs),
+                "kata_kunci": self.kata_kunci(bs),
+                "tahun": self.tahun(bs),
+                "tanggal_register": self.tanggal_register(bs),
+                "lembaga_peradilan": self.lembaga_peradilan(bs),
+                "jenis_lembaga_peradilan": self.jenis_lembaga_peradilan(bs),
+                "hakim_ketua": self.hakim_ketua(bs),
+                "hakim_anggota": self.hakim_anggota(bs),
+                "panitera": self.panitera(bs),
+                "amar": self.amar(bs),
+                "amar_lainnya": self.amar_lainnya(bs),
+                "catatan_amar": self.catatan_amar(bs),
+                "tanggal_musyawarah": self.tanggal_musyawarah(bs),
+                "tanggal_dibacakan": self.tanggal_dibacakan(bs),
+                "kaidah": self.kaidah(bs),
+                "abstrak": self.abstrak(bs)
+            }
+            _json.append(d)
+            self.save_to_json(_json, "data/putusan-per-kasus")
+
+        return _json
 
 
 if __name__ == "__main__":
